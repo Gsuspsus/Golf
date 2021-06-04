db784b793d98e7a3a5f78089be1c0a63aa79dabe
gfalkeenthros <58170723+gfalkeenthros@users.noreply.github.com>
2021-06-04 17:43:40 +0200

    reduced hangman size

--- hangman.rb
+++ hangman.rb
@@ -1,8 +1,2 @@
-w,g,n=$*,[],3;x=w.sample
-loop{
-    p"#{x.chars.map{|c|(g.include? c)?c:?_}.join},#{n}" 
-    break if n<0 or x.chars.all?(&g.method(:include?))
-    $stdin.gets; 
-    g<<(c=$_[0])
-    n-=1 if !x.include?c
-}
+w,g,n=$*,[],4;x=w.sample.chars;
+loop{p"#{x.map{|c|(g.index c)?c:?_}.join},#{n}";break if n<0 or (x&g)==x;g<<(c=$stdin.gets[0]);n-=1 if !x.index c}
